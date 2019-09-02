9cf5bf1609bd958ff17c8002f0859773735c23ef
Kuznecova Nastya <48177932+suova@users.noreply.github.com>
2019-09-02 12:45:51 +0300

    Update sorting.js

--- source/sorting.js
+++ source/sorting.js
@@ -1,7 +1,8 @@
 'use strict';
-const sorting= function(arr,[...field]) {
-  for(let i = [...field].length-1; i >=0; i--) {
-    arr.sort((a, b) => a[[...field][i]]>= b[[...field][i]]? 1 : -1);
-  }
-  return arr;
+let sorting = (arr, fields) => {
+    for(let i = fields.length-1; i >= 0; i--) {
+        arr.sort( (a, b) => a[fields[i]] >= b[fields[i]] ? 1 : -1 );
+    }
+     
+    return arr;
 };
